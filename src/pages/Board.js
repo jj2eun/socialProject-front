c0e3bfcbe999ceb816ba7855e49acf5e1f7ec735
이준성 <jun130506@gmail.com>
2023-03-05 17:22:18 +0900

    임시커밋

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,50 +1,58 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Checkbox from '@mui/material/Checkbox';
-import Avatar from '@mui/material/Avatar';
-import { Button } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  ListItemAvatar,
+  Checkbox,
+  Avatar,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  TextField,
+} from "@mui/material";
 
 const Board = () => {
-
   const [Blist, setBlist] = useState([]);
   const [open, setOpen] = useState(false);
-  const [textContent, setTextContent] = useState('');
+  const [textContent, setTextContent] = useState("");
   const [checked, setChecked] = React.useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     boardList();
-  },[]);
+  }, []);
 
   /*
     리스트 호출
   */
   const boardList = async () => {
-
-    const response = await axios.get('/boardList', {
-        params: {
-            id: '지으닝'
-        }
+    const response = await axios.get("/boardList", {
+      params: {
+        id: "지으닝",
+      },
     });
     setBlist(response.data);
-  }
+  };
 
-  const boardWriting = async () =>{
-    if(textContent !== ""){
-      await axios.post('/boardInsert',{
-        params:{
-          boardId : ''
-          ,id : ''
-          ,contents :''
-        }
-      })
-    }   
-  }
+  const boardWriting = async () => {
+    debugger;
+    console.log(textContent);
+    if (textContent !== "") {
+      await axios.post("/boardInsert", null, {
+        params: {
+          boardId: "1",
+          id: "지으닝",
+          contents: "123",
+        },
+      });
+      setOpen(false);
+    }
+  };
 
   /*
     체크 이벤트 기능 
@@ -65,31 +73,45 @@ const Board = () => {
   /*
     글 쓰기 기능 
   */
-  const handleClickOpen = () =>{
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-  }
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   /*
     글 삭제 기능 
   */
-  const handleRemove = () => {
-    console.log(checked);
-  }
+  const handleRemove = async () => {
+    if (checked.length > 0) {
+      axios.delete("boardDelete", {
+        params: {
+          boardList: checked.map((item) => item.boardSeq).join("|") + "|",
+          id: "지으닝",
+        },
+      });
+    }
+  };
 
-  return(
+  return (
     <>
-      <List dense sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <List
+        dense
+        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+      >
         {Blist.map((value) => {
           const labelId = `checkbox-list-secondary-label-${value}`;
           return (
             <ListItem
-              key={value}
+              key={value.boardSeq}
               secondaryAction={
                 <Checkbox
                   edge="end"
                   onChange={handleToggle(value)}
                   checked={checked.indexOf(value) !== -1}
-                  inputProps={{ 'aria-labelledby': labelId }}
+                  inputProps={{ "aria-labelledby": labelId }}
                 />
               }
               disablePadding
@@ -108,8 +130,33 @@ const Board = () => {
       <Button variant="outlined" onClick={handleRemove}>
         삭제
       </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogContent>
+          <TextField
+            id="outlined-multiline-flexible"
+            label="글작성하기"
+            multiline
+            maxRows={4}
+            onChange={(e) => {
+              setTextContent(e.target.value);
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>취소</Button>
+          <Button onClick={boardWriting} autoFocus>
+            {" "}
+            저장{" "}
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
-  ) 
-}
+  );
+};
 
 export default Board;
